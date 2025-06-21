df32279c88c21f6cd1394273a40fa2186b41d7aa
AshwinDisa <ashwin.disa@gmail.com>
2025-06-20 20:14:57 -0400

    export images

--- clrnet/utils/visualization.py
+++ clrnet/utils/visualization.py
@@ -1,7 +1,5 @@
 import cv2
 import os
-import os.path as osp
-
 
 COLORS = [
     (255, 0, 0),
@@ -36,8 +34,13 @@ COLORS = [
     (128, 0, 255),
 ]
 
+MODE = {"tensorrt": "TensorRT",
+        "pytorch": "PyTorch",
+        "onnx_python_cpu": "ONNX CPU",
+        "onnx_python_gpu": "ONNX GPU",}
 
-def imshow_lanes(img, lanes, show=False, out_file=None, width=4, video=False, fps=None, infer_time=None):
+
+def imshow_lanes(img, lanes, show=False, out_file=None, width=4, video=False, fps=None, infer_time=None, mode=None, frames=None):
     lanes_xys = []
     for _, lane in enumerate(lanes):
         xys = []
@@ -60,18 +63,25 @@ def imshow_lanes(img, lanes, show=False, out_file=None, width=4, video=False, fp
         cv2.waitKey()
 
     if show and video:
+
+        if mode is not None:
+            cv2.putText(img, f"Infer: {MODE[mode]}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX,
+                    1.2, (0, 0, 255), 3, cv2.LINE_AA)
         if fps is not None:
-            cv2.putText(img, f"FPS: {fps}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(img, f"FPS: {fps}", (20, 80), cv2.FONT_HERSHEY_SIMPLEX,
                     1.2, (0, 0, 255), 3, cv2.LINE_AA)
         if infer_time is not None:
-            cv2.putText(img, f"Infer Time: {(infer_time * 1000):.2f} ms", (20, 80), cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(img, f"Infer Time: {(infer_time * 1000):.2f} ms", (20, 120), cv2.FONT_HERSHEY_SIMPLEX,
                     1.2, (0, 0, 255), 3, cv2.LINE_AA)
 
 
-        cv2.imshow('view', img)
-        cv2.waitKey(1)
+        # cv2.imshow('view', img)
+        # cv2.waitKey(1)
 
     if out_file:
-        if not osp.exists(osp.dirname(out_file)):
-            os.makedirs(osp.dirname(out_file))
-        cv2.imwrite(out_file, img)
+        save_path = os.path.join(out_file, f'{mode}')
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        cv2.imwrite(save_path + f'/{frames}.jpg', img)
+        # import pdb
+        # pdb.set_trace()
